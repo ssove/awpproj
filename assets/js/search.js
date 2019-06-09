556b759eb8e7506b9e02e5900bcf6eab248ad767
Lee Minseok <korpenta@gmail.com>
2019-06-10 04:22:04 +0900

    Redirect to stats.html when an ajax request is done

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,13 +6,15 @@ function getCompleteStats() {
         alert("잘못된 배틀태그 입력입니다.");
         return;
     }
+
     let url = getCompleteStatsUrl(battletag[0], battletag[1]);
 
     $.ajax({
         url: url,
         type: "GET",
         success: function(result) {
-            console.log(result);
+            // Save the result into the file.
+            location.href = "stats.html";
         }
     });
 }
@@ -24,4 +26,4 @@ $(document).keypress(function(event) {
         event.preventDefault();
         getCompleteStats();
     }
-})
+});
