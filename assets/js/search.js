b0869e751d2362f8cf0153831bf66f7c50404ab5
Lee Minseok <korpenta@gmail.com>
2019-06-10 03:12:49 +0900

    Handle an event when pressing enter key for request for stats

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,27 @@
-$("#search").click(function() {
+
+function getCompleteStats() {
     let battletag = $("#battletag").val().split("#");
+
+    if (!battletag[0] || !battletag[1]) {
+        alert("잘못된 배틀태그 입력입니다.");
+        return;
+    }
     let url = getCompleteStatsUrl(battletag[0], battletag[1]);
+
     $.ajax({
         url: url,
         type: "GET",
         success: function(result) {
-            console.log(typeof result);
+            console.log(result);
         }
     });
-});
+}
+
+$("#search").click(getCompleteStats);
+
+$(document).keypress(function(event) {
+    if (event.which === 13) {
+        event.preventDefault();
+        getCompleteStats();
+    }
+})
