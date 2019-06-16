350b0c4f9baeb606e5b5ce3b4819bb0638e47ede
Lee Minseok <korpenta@gmail.com>
2019-06-17 05:02:51 +0900

    Change communication structure

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
 
-function getCompleteStats() {
-    let battletag = $("#battletag").val().split("#");
+function getCompleteStats(battletag) {
+    battletag = $("#battletag").val().split("#");
 
     if (!battletag[0] || !battletag[1]) {
         alert("잘못된 배틀태그 입력입니다.");
@@ -18,12 +18,3 @@ function getCompleteStats() {
         }
     });
 }
-
-$("#search").click(getCompleteStats);
-
-$(document).keypress(function(event) {
-    if (event.which === 13) {
-        event.preventDefault();
-        getCompleteStats();
-    }
-});
